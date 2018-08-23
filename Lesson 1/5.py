03694a0d5a80d165951c36763fcf4406c3e21331
a.zorin <a.zorin@qiwi.com>
2018-08-23 15:32:26 +0300

    5 задание

---
+++ Lesson 1/5.py
@@ -0,0 +1,21 @@
+# 5. Пользователь вводит две буквы. Определить, на каких местах алфавита они стоят и сколько между ними находится букв.
+
+import string
+
+a = input(print('Введите первый символ: ')).upper()
+b = input(print('Введите второй символ: ')).upper()
+
+str = string.ascii_uppercase
+
+if len(a) != 1 or len(b) != 1 \
+        or str.find(str) == -1 or str.find(b) == -1:
+    print('Введены некорректные данные')
+else:
+    print(f'{a} находится на {str.find(a)+1} месте в алфавите')
+    print(f'{b} находится на {str.find(b)+1} месте в алфавите')
+    difs = abs(str.find(a) - str.find(b))
+    if difs in (0, 1):
+        difs = 0
+    else:
+        difs -= 1
+    print(f'Между {a} и {b} находится {difs} символа(ов)')
