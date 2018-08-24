407b8ff53c8668fa91afb579e4474ecbf0a7f9c5
a.zorin <a.zorin@qiwi.com>
2018-08-24 09:58:47 +0300

    5 задание

--- Lesson 1/5.py
+++ Lesson 1/5.py
@@ -14,7 +14,7 @@ else:
     print(f'{a} находится на {str.find(a)+1} месте в алфавите')
     print(f'{b} находится на {str.find(b)+1} месте в алфавите')
     difs = abs(str.find(a) - str.find(b))
-    if difs in (0, 1):
+    if difs == 0:
         difs = 0
     else:
         difs -= 1
